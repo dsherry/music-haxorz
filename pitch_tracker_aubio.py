343f5fa2d2bb7131efdf074e8f4849c7db2f333c
Dylan Sherry <sharshofski@gmail.com>
2014-04-27 16:50:04 -0400

    Attempt 1 to use asynchronous pyaudio

--- pitch_tracker_aubio.py
+++ pitch_tracker_aubio.py
@@ -24,6 +24,27 @@ FORMAT = pyaudio.paFloat32
 CHANNELS = 2
 RATE = 44100
 
+# set up callback for writing data
+frameBuffer = []
+def callback(in_data, frame_count, time_info, status):
+    print "Time: %s, status: %s" %(time_info, status)
+    print "Need %d frames" %frame_count
+    ctr = 0
+    while len(frameBuffer) < frame_count:
+        if ctr % 100 == 0: print "Warning: len of frameBuffer is %d" %len(frameBuffer)
+        time.sleep(0.001)
+        ctr += 1
+        if ctr > 1000:
+            print "Fatal error: buffer still empty after 1 second"
+            sys.exit(-1)
+    data = []
+    for i in xrange(frame_count):
+        data.append(frameBuffer.pop(0))
+    joined = b''.join(data)
+    print "Done"
+    print len(joined)
+    return (joined, pyaudio.paContinue)
+
 p = pyaudio.PyAudio()
 instream = p.open(format=FORMAT,
                   channels=CHANNELS,
@@ -33,7 +54,8 @@ instream = p.open(format=FORMAT,
 outstream = p.open(format=FORMAT,
                    channels=CHANNELS,
                    rate=RATE,
-                   output=True)
+                   output=True,
+                   stream_callback = callback)
 
 # set up YIN pitch tracking algo
 tolerance = 0.8
@@ -44,12 +66,17 @@ pitch_o.set_tolerance(tolerance)
 print "Recording"
 
 try:
+    outstream.start_stream()
+    print "Finished?: " + str(outstream.is_active())
     while True:
         data_str = instream.read(hop_s)
         data = numpy.fromstring(data_str,'Float32')
         pitch = pitch_o(data)[0]
         print "Pitch: %.5f Hz"%pitch
-        outstream.write(data_str)
+        for i in xrange(len(data_str)):
+            frameBuffer.append(data_str[i])
+        print "Active?: " + str(outstream.is_active())
+        #outstream.write(data_str)
 except KeyboardInterrupt:
     print "\nReceived interrupt; exiting."
 
